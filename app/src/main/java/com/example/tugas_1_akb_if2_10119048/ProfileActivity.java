4b373c2e9a58ee24c21b93df2e51837c64215396
Mochammad Faishal <faishalm310@gmail.com>
2022-04-24 00:07:43 +0700

    Tugas 1 AKB IF2 10119048

---
+++ app/src/main/java/com/example/tugas_1_akb_if2_10119048/ProfileActivity.java
@@ -0,0 +1,14 @@
+package com.example.tugas_1_akb_if2_10119048;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
